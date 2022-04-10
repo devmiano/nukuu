3854c12ccec80554215cf10deb6eb1d61697b83c
Miano <dev.miano@gmail.com>
2022-04-10 05:59:22 +0300

    Created the quote class

---
+++ src/app/classes/quote.ts
@@ -0,0 +1,11 @@
+export class Quote {
+  public showDetails: boolean;
+  constructor(
+    public id: number,
+    public name: string,
+    public description: string,
+    public creationDate: Date
+  ) {
+    this.showDetails = false;
+  }
+}
