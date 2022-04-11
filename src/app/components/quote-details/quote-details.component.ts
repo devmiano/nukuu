8c8a6f3e9422aa795c3244fa7d1ef62be6a9f944
Miano <dev.miano@gmail.com>
2022-04-11 18:16:56 +0300

    Added the quotes to the quote.component.ts

--- src/app/components/quote-details/quote-details.component.ts
+++ src/app/components/quote-details/quote-details.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Quote } from 'src/app/classes/quote';
 
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
-  styleUrls: ['./quote-details.component.scss']
+  styleUrls: ['./quote-details.component.scss'],
 })
 export class QuoteDetailsComponent implements OnInit {
+  @Input() quote!: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  quoteComplete(complete: boolean): void {
+    this.isComplete.emit(complete);
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
