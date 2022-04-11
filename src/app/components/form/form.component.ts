e1f9f6be777d14af1ca7d45d28e547481c28525d
Miano <dev.miano@gmail.com>
2022-04-11 18:30:13 +0300

    Added the functions for the form component

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Quote } from 'src/app/classes/quote';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+  newQuote = new Quote(0, '', '', '', new Date());
+  @Output() addNewQuote = new EventEmitter<Quote>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submitQuote() {
+    this.addNewQuote.emit(this.newQuote);
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
