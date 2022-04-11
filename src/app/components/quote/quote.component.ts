3067af160ed05c37deb594dc989722e08256376b
Miano <dev.miano@gmail.com>
2022-04-11 18:02:05 +0300

    Added function to create a new quote

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../../classes/quote';
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.scss']
+  styleUrls: ['./quote.component.scss'],
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quote[] = [
+    new Quote(
+      0,
+      'Sammy Muchiri',
+      'Arthur Schopenhauer',
+      'Talent hits a target no one else can hit, Genius hits a target no one else can see.',
+      new Date()
+    ),
+    new Quote(
+      1,
+      'Jane Ngoiri',
+      'Hellen Keller',
+      'Be happy with what you have, while working for what you want.',
+      new Date()
+    ),
+    new Quote(
+      2,
+      'Christian Milan',
+      'Hans Christian Andersen',
+      'Enjoy life. Theres plenty of time to be dead.',
+      new Date()
+    ),
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  toggleDetails(index: number): void {
+    this.quotes[index].showDetails = !this.quotes[index].showDetails;
+  }
+  addNewQuote(quote: Quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength++;
+    quote.creationDate = new Date(quote.creationDate);
+    this.quotes.push(quote);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
